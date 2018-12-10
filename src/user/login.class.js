e4d96b35e103a4a0c56494f6badf50dedea1e447
IDea Factory <dev-team@ideafactory.fr>
2018-12-10 16:12:05 +0100

    Effacement du formulaire et refactorage Login

--- src/user/login.class.js
+++ src/user/login.class.js
@@ -6,8 +6,13 @@ class Login {
         // Modifier le titre de la page
         $('#main-title').html('Identifiez-vous');
 
+        // Définition des attributs
+        this.login = $('[name="loginField"]');
+        this.password = $('[name="passwordField"]');
+
         // Définition du listener sur le formulaire
         this.formListener();
+        this.submitListener();
     }
 
     /**
@@ -16,13 +21,13 @@ class Login {
      * @return void
      */
     formListener() {
+        let login = this.login;
+        let password = this.password;
+
         $('#loginForm').on(
             'keyup',
             // Callback : fonction appelée si l'événement défini survient
             function(event) {
-                // Vérifier le contenu des deux champs
-                const login = $('[name="loginField"]');
-                const password = $('[name="passwordField"]');
 
                 // Est-ce que les deux champs sont remplis
                 if ( 
@@ -32,6 +37,37 @@ class Login {
                     $('#btnLogin').removeAttr('disabled');
                 } else {
                     // Non, ça ne le fait pas tout seul, il faut lui dire
+                    $('#btnLogin').attr('disabled', 'disabled');
+                }
+            }
+        );
+    }
+
+    submitListener() {
+        let login = this.login;
+        let password = this.password;
+
+        $('#loginForm').on(
+            'submit',
+            function(event) {
+                event.preventDefault(); // Empêche l'action par défaut...
+                
+                // Instancie un nouvel utilisateur
+                const user = new User();
+
+                // Définit le login et le password de l'utilisateur
+                user.setUserName(login.val());
+                user.setPassword(password.val());
+
+                // Gère l'authentification...
+                if (user.authenticate() === true) {
+                    console.log('Oki, tu peux y aller');
+                } else {
+                    console.log('ko, t\'as pas le droit !');
+                    // Efface les champs et désactive le bouton
+                    login.val('');
+                    password.val('');
+
                     $('#btnLogin').attr('disabled', 'disabled');
                 }
             }
