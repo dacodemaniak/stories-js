6fadc907c90c86374f0e435dfadde77e005cfdf9
IDea Factory <dev-team@ideafactory.fr>
2018-12-12 13:31:06 +0100

    Refactorage de main

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -3,15 +3,14 @@
  * @desc Point d'entrée principal dans l'application Javascript
  */
 import { Router } from '../../src/modules/router/router.class';
-import { Login } from './../../src/user/login.class';
 import { Route } from '../../src/modules/router/route.class';
 
 // Instancie les routes de l'application
 const router = new Router();
 router
     .add(
-        new Route('/', 'Login')
+        new Route('/', 'LoginController')
     )
     .add(
-        new Route('/home', 'Home')
+        new Route('/mystories', 'MyStories')
     );
